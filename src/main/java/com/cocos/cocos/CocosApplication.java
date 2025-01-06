8878b7094025f40b5695dd47c677b17a14efc9ed
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-01-06 16:24:57 +0900

    initial commit

---
+++ src/main/java/com/cocos/cocos/CocosApplication.java
@@ -0,0 +1,13 @@
+package com.cocos.cocos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocos/cocos/CocosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocos.cocos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
