e0ac5fcb4f70f1aa42df9f047131028fd492ceed
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-01-14 01:50:09 +0900

    Fix: 스웨거 스키마 추가

--- src/main/java/com/cocos/cocos/api/animal/controller/AnimalControllerSwagger.java
+++ src/main/java/com/cocos/cocos/api/animal/controller/AnimalControllerSwagger.java
@@ -3,8 +3,6 @@ package com.cocos.cocos.api.animal.controller;
 import com.cocos.cocos.api.animal.dto.response.AnimalsResponse;
 import com.cocos.cocos.common.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +13,7 @@ public interface AnimalControllerSwagger {
     @Operation(summary = "동물 조회 API", description = "동물을 조회하는 API입니다.")
     @ApiResponse(
             responseCode = "200",
-            description = "동물 조회 성공",
-            content = @Content(mediaType = "application/json", schema = @Schema(implementation = BaseResponse.class)))
+            description = "동물 조회 성공"
+    )
     public ResponseEntity<BaseResponse<AnimalsResponse>> getAnimals();
 }
--- src/main/java/com/cocos/cocos/api/animal/dto/response/AnimalsResponse.java
+++ src/main/java/com/cocos/cocos/api/animal/dto/response/AnimalsResponse.java
@@ -1,8 +1,11 @@
 package com.cocos.cocos.api.animal.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public record AnimalsResponse(
+        @Schema(description = "동물 리스트")
         List<AnimalResponse> animals
 ) {
     public static AnimalsResponse of(final List<AnimalResponse> animals) {
