905bee77a58770d9a1ab4189ec661fa369b328a3
seoyeonjiin <duswls214@khu.ac.kr>
2025-01-17 07:23:18 +0900

    Feat: 애완동물 조회 API 스웨거 구현

--- src/main/java/com/cocos/cocos/api/pet/controller/PetControllerSwagger.java
+++ src/main/java/com/cocos/cocos/api/pet/controller/PetControllerSwagger.java
@@ -1,5 +1,6 @@
 package com.cocos.cocos.api.pet.controller;
 
+import com.cocos.cocos.api.pet.dto.reponse.PetResponse;
 import com.cocos.cocos.api.pet.dto.request.PetCreateRequest;
 import com.cocos.cocos.api.pet.dto.request.PetUpdateRequest;
 import com.cocos.cocos.common.response.BaseResponse;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Pet Controller", description = "애완동물 관련 API")
 public interface PetControllerSwagger {
@@ -30,5 +32,13 @@ public interface PetControllerSwagger {
     public ResponseEntity<BaseResponse<Void>> updatePet(
             @Parameter(name = "petId", description = "애완동물 아이디", in = ParameterIn.PATH, required = true, schema = @Schema(type = "Long")) final Long petId,
             @RequestBody final PetUpdateRequest petUpdateRequest
+    );
+
+    @Operation(summary = "사용자 애완동물 조회 API", description = "사용자 애완동물 조회 API입니다. ")
+    @ApiResponse(
+            responseCode = "200",
+            description = "요청이 성공했습니다.")
+    public ResponseEntity<BaseResponse<PetResponse>> getPet(
+            @RequestParam final String nickname
     );
 }
