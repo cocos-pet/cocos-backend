3f1332c1977e8f36b76bead3fba205c6ad008890
seoyeonjiin <duswls214@khu.ac.kr>
2025-01-18 21:07:18 +0900

    Fix: 닉네임으로 회원 페이지 정보 조회할 수 있도록 변경

--- src/main/java/com/cocos/cocos/api/member/controller/MemberControllerSwagger.java
+++ src/main/java/com/cocos/cocos/api/member/controller/MemberControllerSwagger.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Member Controller", description = "사용자 관련 API")
 public interface MemberControllerSwagger {
@@ -14,5 +15,7 @@ public interface MemberControllerSwagger {
     @ApiResponse(
             responseCode = "200",
             description = "사용자 조회에 성공했습니다.")
-    public ResponseEntity<BaseResponse<MemberProfileResponse>> getMemberProfile(final Long memberId);
+    public ResponseEntity<BaseResponse<MemberProfileResponse>> getMemberProfile(
+            @RequestParam(name = "nickname", required = false) final String nickname
+    );
 }
--- src/main/java/com/cocos/cocos/api/member/service/MemberService.java
+++ src/main/java/com/cocos/cocos/api/member/service/MemberService.java
@@ -18,10 +18,23 @@ public class MemberService {
     private final MemberDataS3Client memberDataS3Client;
 
     @Transactional(readOnly = true)
-    public MemberProfileResponse getMemberProfile(final Long memberId) {
-        final Member member = memberRepository.findById(memberId).orElseThrow(
+    public MemberProfileResponse getMemberProfile(final String nickname, final Long memberId) {
+        final Long selectedMemberId = (nickname != null ) ? findMemberByNickname(nickname): memberId;
+        final Member member = memberRepository.findById(selectedMemberId).orElseThrow(
                 () -> new CocosException(FailMessage.NOT_FOUND_MEMBER)
         );
         return MemberProfileResponse.of(member.getNickname(), memberDataS3Client.getPresignedUrl(member.getImage()));
+    }
+
+    private Long findMemberByNickname(String nickname) {
+        if (nickname != null) {
+            final Member member = memberRepository.findByNickname(nickname);
+            if (member == null) {
+                throw new CocosException((FailMessage.NOT_FOUND_MEMBER));
+            }
+            return member.getId();
+        } else {
+            return null;
+        }
     }
 }
