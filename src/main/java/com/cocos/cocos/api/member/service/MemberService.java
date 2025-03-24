f624ebb69add0d5709633b410c1feb26575de75b
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-03-24 15:37:15 +0900

    Refactor: 테스트 용 import문 삭제

--- src/main/java/com/cocos/cocos/api/member/service/MemberService.java
+++ src/main/java/com/cocos/cocos/api/member/service/MemberService.java
@@ -13,11 +13,9 @@ import com.cocos.cocos.db.member.repository.MemberRepository;
 import com.cocos.cocos.db.member.repository.MemberTokenRepository;
 import com.cocos.cocos.enums.member.Platform;
 import com.cocos.cocos.enums.message.FailMessage;
-import com.cocos.cocos.external.AppDataS3Client;
 import com.cocos.cocos.external.KakaoLoginClient;
 import com.cocos.cocos.external.MemberDataS3Client;
 import lombok.RequiredArgsConstructor;
-import org.springdoc.api.ErrorMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
