991531b388c133ea0b30887f29cff53437e8e032
seoyeonjiin <duswls214@khu.ac.kr>
2025-04-03 22:57:54 +0900

    Refactor: member 객체를 만들지 않고 임의의 memberId를 사용해서 테스트하도록 변경

--- src/test/java/com/cocos/cocos/search/SearchConcurrencyTest.java
+++ src/test/java/com/cocos/cocos/search/SearchConcurrencyTest.java
@@ -2,14 +2,11 @@ package com.cocos.cocos.search;
 
 import com.cocos.cocos.api.search.service.SearchService;
 import com.cocos.cocos.config.JpaAuditingConfig;
-import com.cocos.cocos.db.member.entity.Member;
 import com.cocos.cocos.db.member.repository.MemberRepository;
 import com.cocos.cocos.db.search.repository.SearchRepository;
-import com.cocos.cocos.enums.member.Platform;
 import com.cocos.cocos.enums.search.SearchType;
 import com.cocos.cocos.db.search.entity.Search;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,33 +33,17 @@ class SearchConcurrencyTest {
     @Autowired
     private SearchRepository searchRepository;
 
-    private Long memberId;
-
-    @BeforeEach
-    void setUp() {
-        Member member = Member.builder()
-                .nickname("테스트유저")
-                .email("test@cocos.com")
-                .image("")
-                .platform(Platform.KAKAO)
-                .sub("")
-                .isAdmin(false)
-                .build();
-
-        member = memberRepository.save(member);
-        memberId = member.getId();
-    }
-
     @Test
     @DisplayName("동시에 검색어 저장 요청이 와도 insert는 한 번만 발생한다")
     void addSearchConcurrency() throws InterruptedException {
         // given
-        int threadCount = 10;
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch latch = new CountDownLatch(threadCount);
+        final int threadCount = 10;
+        final ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+        final Long memberId = 1L;
 
-        String keyword = "피부과";
-        SearchType type = SearchType.HOSPITAL;
+        final String keyword = "피부과";
+        final SearchType type = SearchType.HOSPITAL;
 
         // when: 동시에 10개 요청
         for (int i = 0; i < threadCount; i++) {
@@ -82,7 +63,7 @@ class SearchConcurrencyTest {
         }
 
         // then
-        List<Search> results = searchRepository.findAllByMemberIdAndKeywordAndSearchType(memberId, keyword, type);
+        final List<Search> results = searchRepository.findAllByMemberIdAndKeywordAndSearchType(memberId, keyword, type);
         Assertions.assertThat(results).hasSize(1);
     }
 }
