d3255d872d4d7597a14b3e6cd7b1ec0098d6bb54
seoyeonjiin <duswls214@khu.ac.kr>
2025-04-04 16:22:11 +0900

    Feat: Hospital entity에 이미지, 위경도, 리뷰 카운트, 지역 아이디 필드 추가

--- src/main/java/com/cocos/cocos/db/hospital/entity/Hospital.java
+++ src/main/java/com/cocos/cocos/db/hospital/entity/Hospital.java
@@ -1,6 +1,7 @@
 package com.cocos.cocos.db.hospital.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,38 @@ public class Hospital {
 
     @Column(name = "address", nullable = false)
     private String address;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "latitude", nullable = false)
+    private Double latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private Double longitude;
+
+    @Column(name = "review_count", nullable = false)
+    private int reviewCount = 0;
+
+    @Column(name = "town_id", nullable = false)
+    private Long townId;
+
+    @Builder
+    public Hospital(final String name, final String address, final String image, final Double latitude, final Double longitude, final int reviewCount, final Long townId) {
+        this.name = name;
+        this.address = address;
+        this.image = image;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.reviewCount = reviewCount;
+        this.townId = townId;
+    }
+
+    public void addReview() {
+        this.reviewCount++;
+    }
+
+    public void deleteReview() {
+        this.reviewCount--;
+    }
 }
