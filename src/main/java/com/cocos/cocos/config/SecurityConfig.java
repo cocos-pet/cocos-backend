e0c92c7c4fe23b5c8b8585838a1b6bda2550f8ed
seoyeonjiin <duswls214@khu.ac.kr>
2025-04-08 00:07:55 +0900

    Fix: dev에서 비로그인 사용자도 병원 리스트 요청할 수 있도록 white list에 추가

--- src/main/java/com/cocos/cocos/config/SecurityConfig.java
+++ src/main/java/com/cocos/cocos/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
             "/api/dev/test/**",
             "/api/dev/locations",
             "/api/local/hospitals",
+            "/api/dev/hospitals",
             "api/local/members/login"
     };
 
