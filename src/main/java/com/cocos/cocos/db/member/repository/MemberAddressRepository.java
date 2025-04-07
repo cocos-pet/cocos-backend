c87093731230cfa1e09c344e019c93021e5a82f8
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-04-08 03:41:56 +0900

    Fix: MemberAddressRepository에서 사용하지 않는 쿼리문 삭제

--- src/main/java/com/cocos/cocos/db/member/repository/MemberAddressRepository.java
+++ src/main/java/com/cocos/cocos/db/member/repository/MemberAddressRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberAddressRepository extends JpaRepository<MemberAddress, Long> {
-    boolean existsByMemberId(final Long memberId);
-
     MemberAddress findByMemberId(final Long memberId);
 }
