1ddbf05cea9ce9e81e99cec4435f8e749268e05c
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-04-22 18:31:44 +0900

    Feat: 병원 리뷰 생성 및 저장

--- src/main/java/com/cocos/cocos/api/review/service/ReviewService.java
+++ src/main/java/com/cocos/cocos/api/review/service/ReviewService.java
@@ -1,6 +1,8 @@
 package com.cocos.cocos.api.review.service;
 
 import com.cocos.cocos.api.review.dto.response.ReviewAddResponse;
+import com.cocos.cocos.db.review.db.Review;
+import com.cocos.cocos.db.review.repository.ReviewRepository;
 import com.cocos.cocos.enums.pet.Gender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewService {
 
+    private final ReviewRepository reviewRepository;
+
     @Transactional
-    public ReviewAddResponse of(final Long memberId, final Long breedId, final Gender gender,
+    public ReviewAddResponse of(final Long memberId, final Long hospitalId, final Long breedId, final Gender gender,
                                 final Integer weight, final String visitedAt, final String content,
                                 final Long purposeId, final Long diseaseId, final List<Long> symptomIds,
                                 final List<Long> goodReviewIds, final List<Long> badReviewIds, final List<String> images) {
+        final Review review = reviewRepository.save(Review.builder()
+                .breedId(breedId)
+                .gender(gender)
+                .weight(weight)
+                .purposeId(purposeId)
+                .content(content)
+                .hospitalId(hospitalId)
+                .memberId(memberId)
+                .visitedAt(visitedAt)
+                .build());
 
         return null;
     }
