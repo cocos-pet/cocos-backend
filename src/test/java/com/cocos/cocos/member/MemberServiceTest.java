e69c17427e2a37e5b1308c457504a4c7ce450a0d
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-04-28 15:51:01 +0900

    Feat: 리뷰 약관 동의 테스트 작성

---
+++ src/test/java/com/cocos/cocos/member/MemberServiceTest.java
@@ -0,0 +1,45 @@
+package com.cocos.cocos.member;
+
+import com.cocos.cocos.api.member.service.MemberService;
+import com.cocos.cocos.db.member.entity.Member;
+import com.cocos.cocos.db.member.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("지역 서비스 테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("리뷰 약관 동의를 할 수 있다.")
+    void addReviewAgree() {
+        //given
+        final Member member = Member.builder()
+                .build();
+        final Long memberId = 1L;
+
+        ReflectionTestUtils.setField(member, "isReviewTermsAgree", false);
+        ReflectionTestUtils.setField(member, "id", memberId);
+
+        //when
+        memberService.updateReviewTermsAgree(memberId);
+
+        //then
+        Assertions.assertThat(member.getIsReviewTermsAgree()).isEqualTo(true);
+    }
+}
