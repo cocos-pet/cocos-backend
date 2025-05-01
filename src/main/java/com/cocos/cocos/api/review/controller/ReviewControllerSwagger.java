bb5341d6d1dcde16b23327e9b55b0cb7ec53991b
seoyeonjiin <duswls214@khu.ac.kr>
2025-05-01 19:42:05 +0900

    Feat: 사용자 리뷰 리스트 조회 API 스웨거 구현

--- src/main/java/com/cocos/cocos/api/review/controller/ReviewControllerSwagger.java
+++ src/main/java/com/cocos/cocos/api/review/controller/ReviewControllerSwagger.java
@@ -1,19 +1,24 @@
 package com.cocos.cocos.api.review.controller;
 
 import com.cocos.cocos.api.review.dto.request.ReviewAddRequest;
+import com.cocos.cocos.api.review.dto.response.MemberHospitalReviewListResponse;
 import com.cocos.cocos.api.review.dto.response.ReviewAddResponse;
 import com.cocos.cocos.api.review.dto.response.ReviewSummaryListResponse;
 import com.cocos.cocos.api.review.dto.response.ReviewSummaryOptionListResponse;
 import com.cocos.cocos.common.response.BaseResponse;
+import com.cocos.cocos.validation.hospital.HospitalIdConstraint;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Review Controller", description = "리뷰 관련 API")
 public interface ReviewControllerSwagger {
@@ -36,7 +41,7 @@ public interface ReviewControllerSwagger {
     )
     @Parameter(name = "hospitalId", description = "병원 아이디", in = ParameterIn.PATH, required = true, schema = @Schema(type = "Long"))
     public ResponseEntity<BaseResponse<ReviewSummaryListResponse>> getReviewSummaryList(
-            @PathVariable(name = "hospitalId") final Long hospitalId
+            @PathVariable(name = "hospitalId") @HospitalIdConstraint final Long hospitalId
     );
 
     @Operation(summary = "리뷰 요약 옵션 리스트 조회 API", description = "병원 리뷰 요약 옵션 리스트를 조회하는 API입니다. ")
@@ -45,4 +50,18 @@ public interface ReviewControllerSwagger {
             description = "요청에 성공했습니다."
     )
     public ResponseEntity<BaseResponse<ReviewSummaryOptionListResponse>> getReviewSummaryOptionList();
+
+    @Operation(summary = "사용자 리뷰 리스트 조회 API", description = "마이페이지 리뷰 리스트 조회 API입니다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "요청에 성공했습니다."
+    )
+    @Parameter(name = "nickname", description = "사용자 닉네임", required = false)
+    @Parameter(name = "cursorId", description = "페이징용 마지막 리뷰 ID", required = false)
+    @Parameter(name = "size", description = "페이지당 조회할 리뷰 개수 (1~20) 비로그인 시 최대 4개")
+    public ResponseEntity<BaseResponse<MemberHospitalReviewListResponse>> getMemberHospitalReviewList(
+            @RequestParam(name = "nickname", required = false) final String nickname,
+            @RequestParam(name = "cursorId", required = false) final Long cursorId,
+            @RequestParam(name = "size", defaultValue = "10") @Min(value = 1) @Max(value = 20) final int size
+    );
 }
