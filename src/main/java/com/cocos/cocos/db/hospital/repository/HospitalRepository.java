0d18a6d4e06322e55e95480bc3b094f8b4ec3a4f
seoyeonjiin <duswls214@khu.ac.kr>
2025-05-07 03:34:30 +0900

    Fix: locationType, locationId 없이도 오류 없이 hospital list 조회할 수 있도록 수정

--- src/main/java/com/cocos/cocos/db/hospital/repository/HospitalRepository.java
+++ src/main/java/com/cocos/cocos/db/hospital/repository/HospitalRepository.java
@@ -14,7 +14,11 @@ public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 
     List<Hospital> findAllByNameContainingAndDistrictIdInAndIdLessThan(final String name, final List<Long> districtIds, final Long id, final Pageable pageable);
 
+    List<Hospital> findAllByNameContainingAndIdLessThan(final String name, final Long cursorId, final Pageable pageable);
+
     List<Hospital> findAllByNameContainingAndDistrictIdIn(final String name, final List<Long> districtIds, final Pageable pageable);
+
+    List<Hospital> findAllByNameContaining(final String name, final Pageable pageable);
 
     //TODO: 추후 queryDSL 도입 고민 필요
     @Query("""
@@ -26,6 +30,15 @@ public interface HospitalRepository extends JpaRepository<Hospital, Long> {
               )
             """)
     List<Hospital> findAllByDistrictIdInWithCursor(@Param("districtIds") final List<Long> districtIds, @Param("cursorId") final Long cursorId, @Param("reviewCount") final Integer reviewCount, final Pageable pageable);
+
+    @Query("""
+            SELECT h FROM Hospital h
+            WHERE (
+                h.reviewCount < :reviewCount
+                OR (h.reviewCount = :reviewCount AND h.id < :cursorId)
+              )
+            """)
+    List<Hospital> findAllWithCursor(final Long cursorId, final Integer reviewCount, final Pageable pageable);
 
     List<Hospital> findAllByDistrictIdIn(final List<Long> districtIds, final Pageable pageable);
 }
