4e61231d76b86b6dd051e5812033d595ecf7c2ac
seoyeonjiin <duswls214@khu.ac.kr>
2025-05-14 05:36:34 +0900

    Feat: body id 유효성 검증 로직 추가

---
+++ src/main/java/com/cocos/cocos/validation/body/BodyIdValidator.java
@@ -0,0 +1,25 @@
+package com.cocos.cocos.validation.body;
+
+import com.cocos.cocos.common.exception.CocosException;
+import com.cocos.cocos.db.body.repository.BodyRepository;
+import com.cocos.cocos.enums.message.FailMessage;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BodyIdValidator implements ConstraintValidator<BodyIdConstraint, Long> {
+
+    private final BodyRepository bodyRepository;
+
+    @Override
+    public boolean isValid(final Long bodyId, final ConstraintValidatorContext constraintValidatorContext) {
+       if (bodyId == null || bodyRepository.existsById(bodyId)) {
+           return true;
+       } else {
+           throw new CocosException(FailMessage.BAD_REQUEST_INVALID_BODY_ID);
+       }
+    }
+}
