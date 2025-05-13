fc4611c81c95807154cfb75e8fc7046d9e7f75ff
seoyeonjiin <duswls214@khu.ac.kr>
2025-05-14 06:13:35 +0900

    Feat: Purpose id 유효성 검증 로직 추가

---
+++ src/main/java/com/cocos/cocos/validation/purpose/PurposeIdValidator.java
@@ -0,0 +1,25 @@
+package com.cocos.cocos.validation.purpose;
+
+import com.cocos.cocos.common.exception.CocosException;
+import com.cocos.cocos.db.hospital.repository.HospitalVisitPurposeRepository;
+import com.cocos.cocos.enums.message.FailMessage;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PurposeIdValidator implements ConstraintValidator<PurposeIdConstraint, Long> {
+
+    private final HospitalVisitPurposeRepository hospitalVisitPurposeRepository;
+
+    @Override
+    public boolean isValid(final Long purposeId, final ConstraintValidatorContext constraintValidatorContext) {
+        if (purposeId == null || hospitalVisitPurposeRepository.existsById(purposeId)) {
+            return true;
+        }
+        throw new CocosException(FailMessage.BAD_REQUEST_INVALID_PURPOSE_ID);
+    }
+
+}
