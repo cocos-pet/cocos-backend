45de89f8720b97cc275ec61bbd9166da66fbc678
seoyeonjiin <duswls214@khu.ac.kr>
2025-06-14 01:27:56 +0900

    Fix: locationType과 locationId에 대한 검증 로직 추가

--- src/main/java/com/cocos/cocos/api/hospital/dto/request/HospitalListRequest.java
+++ src/main/java/com/cocos/cocos/api/hospital/dto/request/HospitalListRequest.java
@@ -3,20 +3,22 @@ package com.cocos.cocos.api.hospital.dto.request;
 import com.cocos.cocos.enums.hospital.HospitalSortCriteria;
 import com.cocos.cocos.enums.location.LocationType;
 import com.cocos.cocos.validation.hospital.HospitalIdConstraint;
+import com.cocos.cocos.validation.location.HasLocation;
+import com.cocos.cocos.validation.location.LocationConstraint;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
-
 @Builder
+@LocationConstraint
 public record HospitalListRequest(
 
         @Schema(description = "지역 타입",nullable = true, example = "CITY")
         LocationType locationType,
 
         @Schema(description = "지역 아이디", nullable = true, example = "1")
-        Long locationId, // TODO: 유효성 검증 로직 추가
+        Long locationId,
 
         @Schema(description = "마지막으로 조회된 병원 아이디 (첫 요청을 제외하고 필수로 보내야 합니다.)", nullable = true, example = "1")
         @HospitalIdConstraint
@@ -35,5 +37,5 @@ public record HospitalListRequest(
 
         @Schema(description = "정렬 기준", nullable = true, example = "REVIEW")
         HospitalSortCriteria sortBy
-) {
+) implements HasLocation {
 }
