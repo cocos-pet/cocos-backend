feb80a09d43ab0dc39d0f3402755a75e3f5654c4
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2025-06-24 19:54:15 +0900

    Fix: 위치 타입 네이밍 수정

--- src/main/java/com/cocos/cocos/api/location/dto/response/DistrictResponse.java
+++ src/main/java/com/cocos/cocos/api/location/dto/response/DistrictResponse.java
@@ -3,9 +3,9 @@ package com.cocos.cocos.api.location.dto.response;
 public record DistrictResponse(
         Long id,
         String name,
-        String locationType
+        String type
 ) {
-    public static DistrictResponse of(final Long id, final String name, final String locationType) {
-        return new DistrictResponse(id, name, locationType);
+    public static DistrictResponse of(final Long id, final String name, final String type) {
+        return new DistrictResponse(id, name, type);
     }
 }
