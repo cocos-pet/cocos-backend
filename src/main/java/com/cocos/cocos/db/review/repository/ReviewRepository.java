4817b57bf80014bc6038ed3e04483c72d9c6a446
seoyeonjiin <duswls214@khu.ac.kr>
2025-11-21 18:13:15 +0900

    Refactor: 불필요한 메서드 제거

--- src/main/java/com/cocos/cocos/db/review/repository/ReviewRepository.java
+++ src/main/java/com/cocos/cocos/db/review/repository/ReviewRepository.java
@@ -3,7 +3,6 @@ package com.cocos.cocos.db.review.repository;
 import com.cocos.cocos.db.review.db.Review;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,27 +18,4 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByMemberIdAndIdLessThan(final Long memberId, final Long cursorId, final Pageable pageable);
 
     List<Review> findAllByMemberId(final Long memberId, final Pageable pageable);
-
-    @Query("""
-                SELECT DISTINCT r FROM Review r
-                JOIN ReviewSummary s ON r.id = s.reviewId
-                WHERE r.hospitalId = :hospitalId
-                AND (:summaryOptionId IS NULL OR s.reviewSummaryOptionId = :summaryOptionId)
-                AND (:cursorId IS NULL OR r.id < :cursorId)
-            """)
-    List<Review> findByHospitalIdAndSummaryOptionIdAndCursorId(
-            final Long hospitalId, final Long summaryOptionId, final Long cursorId, final Pageable pageable
-    );
-
-    @Query("""
-                SELECT DISTINCT r FROM Review r
-                JOIN ReviewSummary s ON r.id = s.reviewId
-                WHERE (:reviewIds IS NULL OR r.id IN :reviewIds)
-                AND (:summaryOptionId IS NULL OR s.reviewSummaryOptionId = :summaryOptionId)
-                AND (:cursorId IS NULL OR r.id < :cursorId)
-                AND (r.hospitalId IN :hospitalIds)
-            """)
-    List<Review> findByBodyAndLocationAndSummaryOptionIdAndCursorId(
-            final List<Long> reviewIds, final List<Long> hospitalIds, final Long summaryOptionId, final Long cursorId, final Pageable pageable
-    );
 }
