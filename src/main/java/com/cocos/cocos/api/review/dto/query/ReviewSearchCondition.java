bf1c73210a66a972ec76e54f1613be739281200c
seoyeonjiin <duswls214@khu.ac.kr>
2025-11-23 10:59:37 +0900

    Refactor: reviewSearchCondition class -> record 로 변경

--- src/main/java/com/cocos/cocos/api/review/dto/query/ReviewSearchCondition.java
+++ src/main/java/com/cocos/cocos/api/review/dto/query/ReviewSearchCondition.java
@@ -2,16 +2,14 @@ package com.cocos.cocos.api.review.dto.query;
 
 import com.cocos.cocos.enums.location.LocationType;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class ReviewSearchCondition {
-    private Long locationId;
-    private LocationType locationType;
-    private Long hospitalId;
-    private Long summaryOptionId;
-    private Long cursorId;
-    private Long bodyId;
-    private Integer size;
-}
+public record ReviewSearchCondition(
+        Long locationId,
+        LocationType locationType,
+        Long hospitalId,
+        Long summaryOptionId,
+        Long cursorId,
+        Long bodyId,
+        Integer size
+) {}
