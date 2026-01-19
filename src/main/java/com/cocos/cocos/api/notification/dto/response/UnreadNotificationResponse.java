225c2bbd9a9dd6f084227922f7f08eab2fce4a29
seoyeonjiin <duswls214@khu.ac.kr>
2026-01-20 00:36:56 +0900

    Feat: UnreadNotificationResponse 필드 설명 추가

--- src/main/java/com/cocos/cocos/api/notification/dto/response/UnreadNotificationResponse.java
+++ src/main/java/com/cocos/cocos/api/notification/dto/response/UnreadNotificationResponse.java
@@ -1,6 +1,9 @@
 package com.cocos.cocos.api.notification.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UnreadNotificationResponse(
+        @Schema(description = "읽지 않은 알림이 있는지 여부", example = "true")
         boolean hasUnread
 ) {
 }
