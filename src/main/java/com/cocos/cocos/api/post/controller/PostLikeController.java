2032d1c8ddaacd8be5f0a07afa0510140b165a31
seoyeonjiin <duswls214@khu.ac.kr>
2026-01-19 18:41:13 +0900

    Feat: 좋아요 알림 생성 로직 추가 (facade)

--- src/main/java/com/cocos/cocos/api/post/controller/PostLikeController.java
+++ src/main/java/com/cocos/cocos/api/post/controller/PostLikeController.java
@@ -1,5 +1,6 @@
 package com.cocos.cocos.api.post.controller;
 
+import com.cocos.cocos.api.post.facade.PostLikeFacade;
 import com.cocos.cocos.api.post.service.PostLikeService;
 import com.cocos.cocos.common.response.BaseResponse;
 import com.cocos.cocos.common.response.SuccessResponse;
@@ -9,7 +10,12 @@ import com.cocos.cocos.util.validation.EntityExistsValidator;
 import com.cocos.cocos.validation.post.PostIdConstraint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("${api.prefix}/likes")
@@ -18,13 +24,14 @@ public class PostLikeController implements PostLikeControllerSwagger {
 
     private final PostLikeService postLikeService;
     private final EntityExistsValidator entityExistsValidator;
+    private final PostLikeFacade postLikeFacade;
 
     @PostMapping("/{postId}")
     public ResponseEntity<BaseResponse<Void>> addPostLike(
             @PathVariable(name = "postId") @PostIdConstraint final Long postId
     ) {
         entityExistsValidator.validatePetByMemberId(PrincipalHandler.getMemberIdFromPrincipal());
-        postLikeService.addPostLike(PrincipalHandler.getMemberIdFromPrincipal(), postId);
+        postLikeFacade.addPostLikeAndNotification(PrincipalHandler.getMemberIdFromPrincipal(), postId);
         return SuccessResponse.success(SuccessMessage.OK, null);
     }
 
