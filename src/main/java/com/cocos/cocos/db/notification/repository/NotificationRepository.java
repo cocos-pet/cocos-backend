67d535084fcfed55e103ff5793caea63b282c4e9
seoyeonjiin <duswls214@khu.ac.kr>
2026-01-19 18:28:48 +0900

    Feat: Notification repository 추가

---
+++ src/main/java/com/cocos/cocos/db/notification/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.cocos.cocos.db.notification.repository;
+
+import com.cocos.cocos.db.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    boolean existsByPostIdAndMilestone(final Long postId, final int milestone);
+}
