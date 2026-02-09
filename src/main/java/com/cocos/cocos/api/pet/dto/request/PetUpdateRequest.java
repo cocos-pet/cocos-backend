ad7b735b3f224fede627468c650a08a7f013067b
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-09 23:14:51 +0900

    Feat: PastOrPresent 검증 로직 추가

--- src/main/java/com/cocos/cocos/api/pet/dto/request/PetUpdateRequest.java
+++ src/main/java/com/cocos/cocos/api/pet/dto/request/PetUpdateRequest.java
@@ -6,6 +6,7 @@ import com.cocos.cocos.validation.disease.DiseaseIdsConstraint;
 import com.cocos.cocos.validation.symptom.SymptomIdsConstraint;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -27,6 +28,7 @@ public record PetUpdateRequest(
         Integer age, // [TODO] 프론트 배포 후 제거 필요
 
         @Schema(description = "생년월일", example = "2020-01-01", nullable = true)
+        @PastOrPresent
         LocalDate birthDate,
 
         @Schema(description = "질병 아이디 리스트", example = "[1,2,3]", nullable = true)
