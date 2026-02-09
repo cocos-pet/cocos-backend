44145a950e77eadd50cb9c48c8749af3325aedf8
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-09 21:41:31 +0900

    Feat: 펫 응답 DTO에 birthDate 필드 추가

--- src/main/java/com/cocos/cocos/api/pet/dto/response/PetResponse.java
+++ src/main/java/com/cocos/cocos/api/pet/dto/response/PetResponse.java
@@ -3,18 +3,21 @@ package com.cocos.cocos.api.pet.dto.response;
 import com.cocos.cocos.enums.pet.Gender;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record PetResponse(
-        @Schema(description = "애완동물 아이디", example = "1")
+        @Schema(description = "반려동물 아이디", example = "1")
         Long petId,
-        @Schema(description = "애완동물 이미지", example = "url")
+        @Schema(description = "반려동물 이미지", example = "url")
         String petImage,
-        @Schema(description = "애완동물 이름", example = "펫이름")
+        @Schema(description = "반려동물 이름", example = "펫이름")
         String petName,
-        @Schema(description = "애완동물 성별", example = "12")
-        int petAge,
-        @Schema(description = "애완동물 나이", example = "M or F")
+        @Schema(description = "반려동물 나이", example = "12")
+        Integer petAge,
+        @Schema(description = "반려동물 생년월일", example = "2022-02-13")
+        LocalDate petBirthDate,
+        @Schema(description = "반려동물 성별", example = "M or F")
         Gender petGender,
         @Schema(description = "종 아이디", example = "1")
         Long breedId,
@@ -29,7 +32,7 @@ public record PetResponse(
         @Schema(description = "증상 리스트")
         List<PetSymptomResponse> symptoms
 ) {
-    public static PetResponse of(final Long petId, final String petImage, final String petName, final int petAge, final Gender petGender, final Long breedId, final String breed, final Long animalId, final String animal, final List<PetDiseaseResponse> diseases, final List<PetSymptomResponse> symptoms) {
-        return new PetResponse(petId, petImage, petName, petAge, petGender, breedId, breed, animalId, animal, diseases, symptoms);
+    public static PetResponse of(final Long petId, final String petImage, final String petName, final int petAge, final LocalDate petBirtDate, final Gender petGender, final Long breedId, final String breed, final Long animalId, final String animal, final List<PetDiseaseResponse> diseases, final List<PetSymptomResponse> symptoms) {
+        return new PetResponse(petId, petImage, petName, petAge, petBirtDate, petGender, breedId, breed, animalId, animal, diseases, symptoms);
     }
 }
