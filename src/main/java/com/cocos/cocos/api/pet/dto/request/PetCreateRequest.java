857776ab1c615237ba933282a8aab817d76acc50
seoyeonjiin <duswls214@khu.ac.kr>
2026-02-09 21:41:15 +0900

    Feat: 펫 생성 DTO에 birthDate 필드 추가

--- src/main/java/com/cocos/cocos/api/pet/dto/request/PetCreateRequest.java
+++ src/main/java/com/cocos/cocos/api/pet/dto/request/PetCreateRequest.java
@@ -7,9 +7,10 @@ import com.cocos.cocos.validation.symptom.SymptomIdsConstraint;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@Schema(description = "애완동물 생성 형식")
+@Schema(description = "반려동물 생성 형식")
 public record PetCreateRequest(
         @Schema(description = "동물 종 아이디", example = "1")
         @BreedIdConstraint
@@ -21,9 +22,12 @@ public record PetCreateRequest(
         @Schema(description = "성별", example = "F or M")
         Gender gender,
 
-        @Schema(description = "나이", example = "12")
+        @Schema(description = "나이", example = "12", nullable = true)
         @Min(1)
-        int age,
+        Integer age, // [TODO] 프론트 배포 후 제거 필요
+
+        @Schema(description = "생년월일", example = "2020-01-01", nullable = true)
+        LocalDate birthDate,
 
         @Schema(description = "질병 아이디 리스트", example = "[1,2,3]")
         @DiseaseIdsConstraint
